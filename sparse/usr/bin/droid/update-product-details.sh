fdb0c58f512e72258204e8298080d119481a6aae
Dinesh Manajipet <saidinesh5@gmail.com>
2016-12-12 16:25:31 +0530

    Removing the ugly update-product-details hack

--- sparse/usr/bin/droid/update-product-details.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-/bin/sed -i  /deviceManufacturer/s/.*/deviceManufacturer="`getprop ro.product.manufacturer`"/ /var/cache/ssu/board-mappings.ini
-/bin/sed -i /prettyModel/s/.*/prettyModel="`getprop ro.product.model`"/ /var/cache/ssu/board-mappings.ini
